120b505d1bc190bd91938bf00301efc4351bf82e
Frederic Laprade <jean-frederic.laprade@usherbrooke.ca>
2021-01-12 11:25:15 -0500

    Define the plus state measure function

---
+++ main.py
@@ -0,0 +1,11 @@
+import random as rnd
+
+def plus_state_measure():
+    outcomes = [0,1]
+    return rnd.choice(outcomes)
+
+
+if __name__ == '__main__':
+    result = plus_state_measure()
+    print(result)
+
